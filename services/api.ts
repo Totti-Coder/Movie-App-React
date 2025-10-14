52b133f67b0c0d889a219f5ab7d25e564765411b
Pablo <pablogroza2000@gmail.com>
2025-10-14 20:43:34 +0200

    Acabo de modificar las peticiones https a la API para hacer un fetch de la informacion y creo un hook que haga esa captura de datos de una forma mas limpia y escalable

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,6 @@
+// API CONFIGURATION
 export const TMDB_CONFIG = {
-    BASE_URL: "https://api.themoviedb.org/3/",
+    BASE_URL: "https://api.themoviedb.org/3",
     API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
     HEADERS:{
         accept: "application/json",
@@ -7,8 +8,10 @@ export const TMDB_CONFIG = {
     }
 }
 
-export const fetchPopularMovies = async () => {
-    const endpoint = "/discover/movie?sort_by=popularity.desc"
+export const fetchPopularMovies = async ({query}: { query: string}) => {
+    const endpoint = query
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`
 
 
     const response = await fetch(endpoint, {
